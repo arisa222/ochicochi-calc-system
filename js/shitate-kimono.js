76754af478339aef497dc574af5dd05a92401c9f
kawasaki <kawasaki@clood.jp>
2025-02-21 15:36:55 +0900

    上地入れを削除

--- js/shitate-kimono.js
+++ js/shitate-kimono.js
@@ -58,8 +58,6 @@ const preprocessingOptions = [
   new Preprocessing("解き湯のし(絞り仮絵羽)", 5775),
   new Preprocessing("湯通し(三丈もの)紬", 4950),
   new Preprocessing("湯通し(疋)紬", 9900),
-  new Preprocessing("上地入れ(三丈もの)結城など", 7425),
-  new Preprocessing("上地入れ(疋)結城など", 14850),
   new Preprocessing("藍止め(絞り浴衣)", 4950),
   new Preprocessing("藍止め(通常)", 5775)
 ];
